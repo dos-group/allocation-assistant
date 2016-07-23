c7d1449e28a425796da5c74f4ca260536e30a688
Gjum <code.gjum@gmail.com>
2016-07-23 03:06:31 +0200

    System.exit(0) when the job is done
    terminate the application, or else akka will keep it alive

--- src/main/java/de/tuberlin/cit/allocationassistant/AllocationAssistant.java
+++ src/main/java/de/tuberlin/cit/allocationassistant/AllocationAssistant.java
@@ -14,11 +14,16 @@ public class AllocationAssistant {
 		Integer[] scaleOuts = previousRuns.scaleOuts();
 		Double[] runtimes = previousRuns.runtimes();
 
+		System.out.println("found " + scaleOuts.length + " runs with signature " + options.jarWithArgs());
+
 		// then (if multiple previous runs are available) build model (Ilya's code using JBLAS),
 		// then use model to find scale-out (user target if available and between min-max resource constraints)
 		int scaleOut = computeScaleOut(scaleOuts, runtimes, options.maxRuntime());
 
 		new FlinkRunner(options, freamon).runFlink(scaleOut);
+
+		// terminate the application, or else akka will keep it alive
+		System.exit(0);
 	}
 
 	private static int computeScaleOut(Integer[] scaleOuts, Double[] runtimes, int maxRuntime) {
