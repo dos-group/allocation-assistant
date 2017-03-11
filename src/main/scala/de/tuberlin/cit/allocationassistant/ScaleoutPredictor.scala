9f74b7da69dbda70defbc3a3b1b015a2daefe7f5
Gjum <code.gjum@gmail.com>
2017-03-11 23:02:06 +0100

    WIP ScaleoutPredictor: scaleout selection algorithm

--- src/main/scala/de/tuberlin/cit/allocationassistant/ScaleoutPredictor.scala
+++ src/main/scala/de/tuberlin/cit/allocationassistant/ScaleoutPredictor.scala
@@ -1,9 +1,11 @@
 package de.tuberlin.cit.allocationassistant
 
+import java.lang.Double
+
 import akka.pattern.ask
 import akka.util.Timeout
 import com.typesafe.config.Config
-import de.tuberlin.cit.freamon.api.{FindPreviousStages, PreviousRuns, StageDuration}
+import de.tuberlin.cit.freamon.api.{FindPreviousRunsOfStage, PreviousRuns, StageDuration}
 import org.apache.spark.SparkContext
 import org.apache.spark.scheduler.{SparkListener, SparkListenerStageCompleted, SparkListenerStageSubmitted}
 
@@ -14,50 +16,80 @@ import scala.language.postfixOps
 class ScaleoutPredictor(
                          sparkContext: SparkContext,
                          appSignature: String,
+                         numStages: Int,
                          inputSize: Double,
+                         targetRuntime: Int,
                          clusterConfig: Config
                        ) extends SparkListener {
 
-  private val akkaConfig = clusterConfig.getConfig("ScaleoutPredictor.akka")
 
-  private val scaleoutConstraint = (
-    clusterConfig.getInt("ScaleoutPredictor.cluster.minExecutors"),
-    clusterConfig.getInt("ScaleoutPredictor.cluster.maxExecutors"))
+  private val minExecutors = clusterConfig.getInt("ScaleoutPredictor.cluster.minExecutors")
+  private val maxExecutors = clusterConfig.getInt("ScaleoutPredictor.cluster.maxExecutors")
 
-  private val freamon = new Freamon(akkaConfig)
+  private val freamon = new Freamon(clusterConfig.getConfig("ScaleoutPredictor.akka"))
 
+  private var jobStart: Long = 0
   private var currentStageNr = 1
   private var stageStartTime: Long = 0
   private var currentScaleout: Int = 0
 
-  private var futurePrevStages: Future[PreviousRuns] = _
+  private var futureStageRuntimes: Map[Int, Future[PreviousRuns]] = _
 
   override def onStageSubmitted(stageSubmitted: SparkListenerStageSubmitted): Unit = {
-    // TODO make this work for multiple stages running in parallel
+    // we assume that at most one stage runs at any given time
     currentStageNr += 1
     stageStartTime = System.currentTimeMillis()
+    if (jobStart == 0)
+      jobStart = stageStartTime
   }
 
   override def onStageCompleted(stageCompleted: SparkListenerStageCompleted): Unit = {
     val stageStopTime = System.currentTimeMillis()
-
-    val newScaleout = predictNewScaleout(currentStageNr + 1)
-    val ok = sparkContext.requestTotalExecutors(newScaleout, 0, null)
+    val stageDuration = stageStopTime - stageStartTime
 
     recordStageTimes(stageStartTime, stageStopTime, currentScaleout)
-    currentScaleout = newScaleout
 
-    val request = FindPreviousStages(appSignature, currentStageNr + 1)
+    val nextStages = currentStageNr + 1 until numStages
+    val stageRuntimes = futureStageRuntimes.mapValues(getStageRuntimes)
+
+    // check if the job will finish in time, rescale if it won't
+    val runtimeLeft = jobStart + targetRuntime - System.currentTimeMillis()
+    val totalDurationNeededForCurrentScaleout = nextStages
+      .map(predictStageDurationForScaleout(_, currentScaleout, stageRuntimes))
+      .sum
+    if (totalDurationNeededForCurrentScaleout > runtimeLeft * 1.05) {
+      // exceeding target runtime by over 5%, rescaling
+
+      val newScaleout = (minExecutors to maxExecutors)
+        .find { scaleout =>
+          val totalDurationNeeded = nextStages
+            .map(predictStageDurationForScaleout(_, scaleout, stageRuntimes))
+            .sum
+          totalDurationNeeded < runtimeLeft * .90 // 10% slack
+        }
+        .getOrElse(maxExecutors)
+
+      val ok = sparkContext.requestTotalExecutors(newScaleout, 0, null)
+      currentScaleout = newScaleout
+    }
+
     implicit val timeout = Timeout(999 seconds)
-    futurePrevStages = (freamon.freamonMaster ? request).asInstanceOf
+    futureStageRuntimes = nextStages.map { n =>
+      (n, freamon.freamonMaster ? FindPreviousRunsOfStage(appSignature, n))
+    }.toMap.asInstanceOf
   }
 
-  def predictNewScaleout(stage: Int): Int = {
-    val prevStages = Await.result(futurePrevStages, 5 seconds)
-    val (scaleOuts, runtimes) = AllocationAssistant.filterPreviousRuns(prevStages, inputSize)
-    val targetRuntime = ???
-    val newScaleout = AllocationAssistant.computeScaleOut(scaleOuts, runtimes, scaleoutConstraint, targetRuntime)
-    newScaleout
+  private def getStageRuntimes(response: Future[PreviousRuns]): Array[java.lang.Double] = {
+    val runsOfSameStage = Await.result(response, 0 seconds)
+    runsOfSameStage.datasetSizes.zip(runsOfSameStage.runtimes)
+      .filter { case (size, runtime) =>
+        .9 * inputSize < size && size < 1.1 * inputSize
+      }.map(_._2)
+  }
+
+  def predictStageDurationForScaleout(stage: Int, scaleout: Int, stageRuntimes: Map[Int, Array[java.lang.Double]]): Int = {
+    // TODO queries the model in a different way than the allocation assistant
+    ???
   }
 
   def recordStageTimes(stageStart: Long, stageStop: Long, numExecutors: Int): Unit = {
